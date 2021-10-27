7d8ceeaf7d6b58c93a9e4a5bf8740a22e0a675f8
Petr Fila <petrfila@192-168-1-6.tpgi.com.au>
2021-10-27 20:12:06 +1100

    added an error message to the redirection

--- search/views.py
+++ search/views.py
@@ -3,22 +3,16 @@ from search.forms import SearchForm
 from django.contrib import messages
 
 
-
 def search_form(request):
-    print(request)
     s_form = SearchForm
     return render(request, 'search_form.html', {'form': s_form})
 
 
 def find_title(request):
     if request.method == 'POST':
-        print(request)
-
         # titles = Title.objects.get()
         return render(request, 'display_results.html')
     else:
         # Render the search form again and display some extra friendly error message
-        print('Not POST method')
         messages.error(request, 'Sorry mate. No search request has been made.')
-
         return redirect('/')
