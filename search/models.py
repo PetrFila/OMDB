e7be5306ab9cf83dbf0b5d12233be7082ce883ab
Petr Fila <petrfila@192-168-1-6.tpgi.com.au>
2021-10-27 19:51:42 +1100

    added redirection back to the search page if wrong HTTP method

--- search/models.py
+++ search/models.py
@@ -8,8 +8,8 @@ class Title(models.Model):
     )
     # The first item in the (tuple) is the value and second is the label to be shown in the dropdown menu
 
-    title = models.CharField(max_length=200)
-    type = models.CharField(max_length=20, choices=title_types)
+    title = models.CharField(max_length=200, blank=True)
+    type = models.CharField(max_length=20, choices=title_types, blank=True)
     year = models.CharField(max_length=30)
     genre = models.CharField(max_length=30)
     director = models.CharField(max_length=100)
--- search/views.py
+++ search/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from search.forms import SearchForm
+from django.contrib import messages
+
 
 
 def search_form(request):
+    print(request)
     s_form = SearchForm
     return render(request, 'search_form.html', {'form': s_form})
 
 
 def find_title(request):
-    # return HttpResponse('This is the title you have searched for')
-    # titles = Title.objects.get()
-    return render(request, 'display_results.html')
+    if request.method == 'POST':
+        print(request)
+
+        # titles = Title.objects.get()
+        return render(request, 'display_results.html')
+    else:
+        # Render the search form again and display some extra friendly error message
+        print('Not POST method')
+        messages.error(request, 'Sorry mate. No search request has been made.')
+
+        return redirect('/')
